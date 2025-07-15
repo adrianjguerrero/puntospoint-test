082ec4f07f28e5fdda5540ac46abf5f3eeccfbb8
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-07-15 13:25:04 -0400

    created a auth checker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+
+  def authenticate_user_or_unauthorized
+    unless current_user
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
