0d9dfca07e18a2bea12066ba3bb65ef420f98688
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-07-15 16:08:30 -0400

    test for emailt

--- test/controllers/sale_controller_spec.rb
+++ test/controllers/sale_controller_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 RSpec.describe "Sales API", type: :request do
   describe "POST /sales" do
     let(:admin_user) { Administrator.create(email: "admin@example.com", password: "password") }
+    let(:second_admin_user) { Administrator.create(email: "admin2@example.com", password: "password") }
     let(:product) { Product.create(name: "Test Product", stock: 1, price: 100, user: admin_user) }
+    let(:product_mail) { Product.create(name: "Mail Product", stock: 2, price: 100, user: admin_user) }
     let(:sale_params) do
       {
         sale: [
@@ -11,10 +13,17 @@ RSpec.describe "Sales API", type: :request do
         ]
       }
     end
+    let(:mail_sale_params) do
+      {
+        sale: [
+          { product_id: product_mail.id, quantity: 1 }
+        ]
+      }
+    end
 
     let(:user) { User.create(email: "test@example.com", password: "password") }
     let(:auth_token) do
-      # log usern
+      # log user
       post "/login", params: {
         user: {
           email: user.email,
@@ -48,6 +57,28 @@ RSpec.describe "Sales API", type: :request do
       expect(product.stock).to eq(0)
 
       expect(Sale.count).to eq(1)
+    end
+
+    it "sends an email only the first time a product is purchased, even in race conditions" do
+      # clear emails before the test
+      ActionMailer::Base.deliveries.clear
+
+      threads = []
+      2.times do
+        threads << Thread.new do
+          post "/sales", params: mail_sale_params, headers: { Authorization: auth_token }
+        end
+      end
+
+      threads.each(&:join)
+
+      # check that only one email was sent
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+
+      # check email content
+      email = ActionMailer::Base.deliveries.first
+      expect(email.to).to include(admin_user.email)
+      expect(email.subject).to eq("Primer venta del producto: #{product_mail.name}")
     end
   end
 end
