e68ea6d0268855fe89159006c24794cdf870afdf
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-07-15 20:39:33 -0400

    created stadistic controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   # Ruta protegida para crear una venta
   authenticate :user do
     post "sales", to: "sale#create"
+    get "most_sold_products", to: "stadistics#most_purchased_products_by_category"
+    get "top_revenue_products", to: "stadistics#top_revenue_products"
+    get "purchases_by_parameters", to: "stadistics#purchases_by_parameters"
+    get "purchases_by_granularity", to: "stadistics#purchases_by_granularity"
   end
 
 end
